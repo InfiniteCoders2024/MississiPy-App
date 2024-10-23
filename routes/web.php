18620fc48060b64151a93f6240f9ec3cb5ccb771
JoaFal <joaquim.falcao@gmail.com>
2024-10-23 22:31:22 +0100

    Update web.php
    Reorganização das Rotas

--- routes/web.php
+++ routes/web.php
@@ -19,68 +19,94 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__ . '/auth.php';
 
-// Rotas for Models ------------------------------------------------------------
+
+// Routes for Models ----------------------------------------------------------------------
 
 // Author
 use App\Http\Controllers\AuthorController;
 
 Route::get('/author', [AuthorController::class, 'index']);
+Route::post('/author', [AuthorController::class, 'store'])->name('mississipy.author.store');
+Route::get('/author/create', [AuthorController::class, 'create'])->name('mississipy.author.create');
 
-
-Route::get('/author/create', [AuthorController::class, 'create'])->name('author.create');
 
 // Book
 use App\Http\Controllers\BookController;
 
 Route::get('/book', [BookController::class, 'index']);
+Route::post('/book', [BookController::class, 'store'])->name('mississipy.book.store');
+Route::get('/book/create', [BookController::class, 'create'])->name('mississipy.book.create');
+
 
 // BookAuthor
 use App\Http\Controllers\BookAuthorController;
 
 Route::get('/bookauthor', [BookAuthorController::class, 'index']);
 
+
 // Client
 use App\Http\Controllers\ClientController;
 
 Route::get('/client', [ClientController::class, 'index']);
+Route::post('/client', [ClientController::class, 'store'])->name('mississipy.client.store');
+Route::get('/client/create', [ClientController::class, 'create'])->name('mississipy.client.create');
+
 
 // Electronic
 use App\Http\Controllers\ElectronicController;
 
 Route::get('/electronic', [ElectronicController::class, 'index']);
+Route::post('/electronic', [ElectronicController::class, 'store'])->name('mississipy.electronic.store');
+Route::get('/electronic/create', [ElectronicController::class, 'create'])->name('mississipy.electronic.create');
+
 
 // Operator
 use App\Http\Controllers\OperatorController;
 
 Route::get('/operator', [OperatorController::class, 'index']);
+Route::post('/operator', [OperatorController::class, 'store'])->name('mississipy.operator.store');
+Route::get('/operator/create', [OperatorController::class, 'create'])->name('mississipy.operator.create');
+
 
 // Order
 use App\Http\Controllers\OrderController;
 
 Route::get('/order', [OrderController::class, 'index']);
+Route::post('/order', [OrderController::class, 'store'])->name('mississipy.order.store');
+Route::get('/order/create', [OrderController::class, 'create'])->name('mississipy.order.create');
+
 
 // Ordered_item
 use App\Http\Controllers\OrderedItemController;
 
 Route::get('/ordered_item', [OrderedItemController::class, 'index']);
+Route::post('/ordered_item', [OrderedItemController::class, 'store'])->name('mississipy.ordered_item.store');
+Route::get('/ordered_item/create', [OrderedItemController::class, 'create'])->name('mississipy.ordered_item.create');
+
 
 // Product
 use App\Http\Controllers\ProductController;
 
 Route::get('/product', [ProductController::class, 'index']);
+Route::post('/product', [ProductController::class, 'store'])->name('mississipy.product.store');
+Route::get('/product/create', [ProductController::class, 'create'])->name('mississipy.product.create');
 
-// Route for NavBar Pesquisa e CheckOut -------------------------------------------------------------
-
-Route::get('/searchBar', [ProductController::class, 'searchBar'])->name('searchBar');
-
-// Route::get('/checkout', [OrderController::class, 'checkout'])->name('checkout');
 
 // Recommendation
 use App\Http\Controllers\RecommendationController;
 
 Route::get('/recommendation', [RecommendationController::class, 'index']);
+Route::post('/recommendation', [RecommendationController::class, 'store'])->name('mississipy.recommendation.store');
+Route::get('/recommendation/create', [RecommendationController::class, 'create'])->name('mississipy.recommendation.create');
 
-// Rotas for Views --------------------------------------------------------------
+
+// Route for NavBar Pesquisa e CheckOut -------------------------------------------------- 
+
+Route::get('/searchBar', [ProductController::class, 'searchBar'])->name('searchBar');
+// Route::get('/checkout', [OrderController::class, 'checkout'])->name('checkout');
+
+
+// Routes for Views ----------------------------------------------------------------------
 
 // welcome_client
 Route::get('/welcome_client', function () {
@@ -101,42 +127,3 @@ Route::get('/product_detail', function () {
 Route::get('/checkout', function () {
     return view('checkout');
 });
-
-
-// Rotas for Forms --------------------------------------------------------------
-
-// Author
-Route::get('/author/create', [AuthorController::class, 'create'])->name('mississipy.author.create');
-Route::post('/author', [AuthorController::class, 'store'])->name('mississipy.author.store');
-
-// Book
-Route::get('/book/create', [BookController::class, 'create'])->name('mississipy.book.create');
-Route::post('/book', [BookController::class, 'store'])->name('mississipy.book.store');
-
-// Client
-Route::get('/client/create', [ClientController::class, 'create'])->name('mississipy.client.create');
-Route::post('/client', [ClientController::class, 'store'])->name('mississipy.client.store');
-
-// Electronic
-Route::get('/electronic/create', [ElectronicController::class, 'create'])->name('mississipy.electronic.create');
-Route::post('/electronic', [ElectronicController::class, 'store'])->name('mississipy.electronic.store');
-
-// Operator
-Route::get('/operator/create', [OperatorController::class, 'create'])->name('mississipy.operator.create');
-Route::post('/operator', [OperatorController::class, 'store'])->name('mississipy.operator.store');
-
-// Order
-Route::get('/order/create', [OrderController::class, 'create'])->name('mississipy.order.create');
-Route::post('/order', [OrderController::class, 'store'])->name('mississipy.order.store');
-
-// Ordered item
-Route::get('/ordered_item/create', [OrderedItemController::class, 'create'])->name('mississipy.ordered_item.create');
-Route::post('/ordered_item', [OrderedItemController::class, 'store'])->name('mississipy.ordered_item.store');
-
-// Product
-Route::get('/product/create', [ProductController::class, 'create'])->name('mississipy.product.create');
-Route::post('/product', [ProductController::class, 'store'])->name('mississipy.product.store');
-
-// Recommendation
-Route::get('/recommendation/create', [RecommendationController::class, 'create'])->name('mississipy.recommendation.create');
-Route::post('/recommendation', [RecommendationController::class, 'store'])->name('mississipy.recommendation.store');
