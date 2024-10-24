96e066eb3800158523abe6a3b325b05867b5e4c1
MCMiguel <106669262+MCMiguel@users.noreply.github.com>
2024-10-24 23:13:56 +0100

    Actualizado Logo2

--- resources/views/mississipy/author/index.blade.php
+++ resources/views/mississipy/author/index.blade.php
@@ -1,4 +1,4 @@
-<style>
+{{-- <style>
     body {
         background-color: #F0F4F8;
     }
@@ -21,27 +21,40 @@
     td {
         color: #4A5568;
     }
-</style>
+</style> --}}
+<head>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
 
-<h1>Author List</h1>
+<div class="container mt-5">
+    <h1 class="text-center mb-4">Author List</h1>
 
-<table>
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Full Name</th>
-            <th>Birthdate</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($authors as $author)
-            <tr>
-                <td>{{ $author->id }}</td>
-                <td>{{ $author->name }}</td>
-                <td>{{ $author->fullname }}</td>
-                <td>{{ $author->birthdate }}</td>
-            </tr>
-        @endforeach
-    </tbody>
+    <div class="table-responsive">
+        <table class="table table-striped table-bordered table-hover">
+            <thead class="table-dark">
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Full Name</th>
+                    <th>Birthdate</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($authors as $author)
+                    <tr>
+                        <td>{{ $author->id }}</td>
+                        <td>{{ $author->name }}</td>
+                        <td>{{ $author->fullname }}</td>
+                        <td>{{ $author->birthdate }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
 </table>
+<!-- Bootstrap JS and Popper.js -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
