7b6eaf0806e5f9ef080c1983c0a70b0d7e88d192
JoaFal <joaquim.falcao@gmail.com>
2024-10-26 22:01:38 +0100

    searchBar: Gerar Tabela
    Alteradas as querys que geram a Lista para ser filtrada pelas buscas do Cliente

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,21 +11,20 @@
 
 <body class="font-sans antialiased">
 
-    <div class="container mt-4">
-        @yield('content')
-    </div>
     <!-- Page Heading -->
     @isset($header)
-        <header class="bg-white dark:bg-gray-800 shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
-            </div>
-        </header>
+    <header class="bg-white dark:bg-gray-800 shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            {{ $header }}
+        </div>
+    </header>
     @endisset
-
+    
     <!-- Page Content -->
     <main>
-        {{ $slot }}
+        <div class="container mt-4">
+            @yield('content')
+        </div>
     </main>
     </div>
 </body>
